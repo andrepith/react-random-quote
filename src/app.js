df4a7196295fcc73bf56a1978f41a4733a37d03c
Andre <andrepith@yahoo.co.uk>
2018-08-12 16:32:27 +0700

    Think Later App

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ThinkLater from './components/ThinkLater';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<ThinkLater />, document.getElementById('app'));
